24307e2580b45069912ec41851b680df008fd6d7
mahajrod <mahajrod@gmail.com>
2021-03-06 21:01:59 +0700

    fixes

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_list_of_files(list_of_dirs_and_files, expression=None, recursive=True):
 
 
 setup(name='KrATER',
-      version='2.2b',
+      version='2.3',
       packages=find_packages(),
       author='Sergei F. Kliver',
       url='https://github.com/mahajrod/KRATER',
